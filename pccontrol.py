7155d1f7e24be564bdbcc886f03c0ab12b518676
bolkop <44177343+bolkop@users.noreply.github.com>
2019-02-02 08:59:45 +0000

    Update pccontrol.py

--- pccontrol.py
+++ pccontrol.py
@@ -3,84 +3,66 @@ import wmi
 
 
 class pccontrol:
-    
-    
+      
     def __init__(self):
-        None
+        self.localInterfaces = []
+        self.getNicDetis()
     
     
-    def get_MACIP(self):
+    def getNicDetis(self):
         """
-        Obtain MAC addresses and associated IP addresses for host PC
-        OUT: Dictionary of MAC addresses as key and IP addresses as value. Only active NIC are searched
-        Raise exception if not able to execute command
+        Obtain MAC, IP Mask and Default Gateway from host PC
         """
-        macIP = {}
-        ipAddresses = []
-        try:
-            connection = wmi.WMI () 
-            for interface in connection.Win32_NetworkAdapterConfiguration (IPEnabled=1):
-                for ip in interface.IPAddress:
-                    ipAddresses.append(ip)
-                macIP[interface.MACAddress] = ipAddresses
-            return macIP
-        except Exception as e:
-            return e
-    
-    def get_MAC(self):
+        connection = wmi.WMI () 
+        for interface in connection.Win32_NetworkAdapterConfiguration (IPEnabled=1):
+            for ip in interface.IPAddress:            
+                self.localInterfaces.append(nic(interface.MACAddress, interface.IPAddress, interface.IPSubnet, interface.DefaultIPGateway))
+        else:
+            self.localInterfaces.append(nic())
+    def getMacAddresses(self):
         """
         Obtain MAC addresses of host PC
         OUT: List of MAC addresses of active NIC
-        Raise exception if not able to execute command
         """
-        #macAddresses = []
-        try:
-            #connection = wmi.WMI () 
-            #network_devices = connection.Win32_NetworkAdapterConfiguration (IPEnabled=1)
-            # if len(network_devices) == 0:
-                # raise ValueError("No active network devices")
-            
-            # if len(network_devices) > 1:
-                # raise ValueError("More than one network device is active")
-            
-          
-            # for interface in connection.Win32_NetworkAdapterConfiguration (IPEnabled=1):
-                # macAddresses.append(interface.MACAddress)
+        macAddresses = []     
+        for i in self.localInterfaces:            
+            macAddresses.append(i.macAddress)
+            return macAddresses
+      
+    def getIp(self, mac):
+        """
+        Obtain IP address for provided MAC Address
+        IN: MAC Address as string
+        OUT: IP address
+        """          
+        for i in self.localInterfaces:
+            if i.macAddress == mac:
+                return i.ipAddress
+               
+    def getSubnet(self, ip):
+        """
+        Obtain subnet mask for provided IP address
+        IN: IP Address as string
+        OUT: Subnet Mask
+        """  
+        for i in self.localInterfaces:
+            for index, t in enumerate(i.ipAddress):
+                if t == ip:
+                    return i.subnetMask[index]
                 
-            #print (macAddresses)
-            
-            return self.get_MACIP().keys()
-        except Exception as e:
-            return e
-    
-    
-        
-    def get_IP(self, macAddress):
+    def getDefaultGateway(self, ip):
         """
-        Obtain IP address for provide macAddress
-        IN: macAddress as string
-        OUT: List of IP addresses
-        Raise exception if not able to execute command
-        """
-        #ipAddresses = []
-        try:
-            # connection = wmi.WMI ()     
-            # for interface in connection.Win32_NetworkAdapterConfiguration (IPEnabled=1):
-                # print(interface.MACAddress)
-                # if interface.MACAddress == macAddress:
-                    # for ip in interface.IPAddress:
-                        # ipAddresses.append(ip)
-            
-            # Obtain all active MAC addresses            
-            macIP = self.get_MACIP()
-            for mac in macIP.keys():
-            # Return all IP addresses for the matching MAC address   
-                if mac == macAddress:    
-                    return macIP[mac]
-        except Exception as e:
-            return e
+        Obtain default gateway for provided IP address
+        IN: IP Address as string
+        OUT: Default Gateway
+        """  
+        for i in self.localInterfaces:
+            for index, t in enumerate(i.ipAddress):
+                if t == ip:                       
+                    return i.defaultGateway[index]
+              
     
-    def set_IP(self, mac, ip = None, subnetmask = None, gateway = None):
+    def setIp(self, mac, ip = None, subnetmask = "255.255.255.0", gateway = None):
         """
         NIC with MACAddress is set to IP address as provided value ip. 
         If ip is None, IP address is set automatically, DHCP is enabled      
@@ -91,61 +73,95 @@ class pccontrol:
         try:  
             connection = wmi.WMI () 
             for interface in connection.Win32_NetworkAdapterConfiguration (IPEnabled=1):
-                print(interface.MACAddress)
-                if interface.MACAddress == mac:       
+                if interface.MACAddress == mac:                     
                     if ip is None:
-                        return interface.EnableDHCP()                                                        
+                        interface.EnableDHCP() 
+                        self.getNicDetis()
+                        
                     # Set IP address, subnetmask and default gateway
                     # Note: EnableStatic() and SetGateways() methods require *lists* of values to be passed
-                    else:
+                    else:                        
                         interface.EnableStatic(IPAddress=[ip],SubnetMask=[subnetmask])
                         if gateway is True:
                             interface.SetGateways(DefaultIPGateway=[gateway])
                         else:
                             interface.SetGateways(DefaultIPGateway=["0.0.0.0"])
-                    return self.get_IP(mac)
                     
+                        self.getNicDetis()            
+                                           
         except Exception as e:
             return e
     
+   
  #********************** End of class pccontrol ********************************          
 
  
-#hostPC = pccontrol()
-#print (hostPC.get_MAC())
-
-#print (hostPC.get_IP("CC:3D:82:85:BC:77"))
-#print (hostPC.get_IP("48:0F:CF:69:3B:2F"))
-#print (hostPC.get_MACIP())  
-#print(hostPC.set_IP(u"48:0F:CF:69:3B:2F", u"192.168.1.1", u"255.255.255.0"))
-#print(hostPC.set_IP("48:0F:CF:69:3B:2F"))
-
+class nic:
     
-#print(set_IP(u"48:0F:CF:69:3B:2F", u"192.168.1.114", u"255.255.255.0"))
+    def __init__(self, mac=None, ip=None, mask=None, gateway=None):
+        self.macAddress = mac
+        self.ipAddress = ip
+        self.subnetMask = mask
+        self.defaultGateway = gateway        
+                
+ #********************** End of class nic ********************************            
 
-# c = wmi.WMI () 
-# interface_name = []
-# mac_addresses = []
-# ip_addresses = []
-# mac_ip = {}
 
-# network_devices = c.Win32_NetworkAdapterConfiguration (IPEnabled=1)
-# if len(network_devices) == 0:
-    # print("No active network devices")
+if __name__ == "__main__": 
+    import time
+    def temp(mac, ip=None):
+        hostPC.setIp(mac, ip)
+        hostPC.getNicDetis()
+        print (hostPC.getIp(mac))        
+     
+     
+    hostPC = pccontrol()
+    print (hostPC.getMacAddresses()[0])
+    mac = hostPC.getMacAddresses()[0]
+    hostPC.setIp(mac)
+  #  time.sleep(8)
+  #  print (hostPC.getIp(mac))
+  #  print (hostPC.getSubnet("144.179.171.66"))
+    print (hostPC.getDefaultGateway("144.179.171.66"))
+   # temp(mac)
 
-# if len(network_devices) > 1:
-    # print ("More than one network device is active")
-    
-# nic = network_devices[0]
-# for ip_address in nic.IPAddress:
-  # #  print (ip_address)
-    # ip_addresses.append(ip_address)
-       
-# mac_ip[nic.MACAddress] = ip_addresses
-# print(mac_ip)
-    
-# for key in mac_ip:
-    # if len(mac_ip[key])>1:
-        # print ("mac address: %s has more than one IP address: %s" %(key, mac_ip[key]))
-# mac_ip
-# ip_addresses
+
+            
+
+
+    #print (hostPC.get_IP("CC:3D:82:85:BC:77"))
+    #print (hostPC.get_IP("48:0F:CF:69:3B:2F"))
+    #print (hostPC.get_MACIP())  
+    #print(hostPC.set_IP(u"48:0F:CF:69:3B:2F", u"192.168.1.1", u"255.255.255.0"))
+    #print(hostPC.set_IP("48:0F:CF:69:3B:2F"))
+
+        
+    #print(set_IP(u"48:0F:CF:69:3B:2F", u"192.168.1.114", u"255.255.255.0"))
+
+    # c = wmi.WMI () 
+    # interface_name = []
+    # mac_addresses = []
+    # ip_addresses = []
+    # mac_ip = {}
+
+    # network_devices = c.Win32_NetworkAdapterConfiguration (IPEnabled=1)
+    # if len(network_devices) == 0:
+        # print("No active network devices")
+
+    # if len(network_devices) > 1:
+        # print ("More than one network device is active")
+        
+    # nic = network_devices[0]
+    # print (nic)
+    # for ip_address in nic.IPSubnet:
+        # print (ip_address)
+        # ip_addresses.append(ip_address)
+           
+    # mac_ip[nic.MACAddress] = ip_addresses
+    # print(mac_ip)
+        
+    # for key in mac_ip:
+        # if len(mac_ip[key])>1:
+            # print ("mac address: %s has more than one IP address: %s" %(key, mac_ip[key]))
+    # mac_ip
+    # ip_addresses
